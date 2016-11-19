998b6fffbdf737fdeea589e78d56b4e29ca76bd2
Israel Andrade <israelandrade18957@student.hartnell.edu>
2016-11-19 01:17:45 -0800

    Merge MainActivity and StartActivity

--- HangMan/app/src/main/java/com/example/hangman/MainActivity.java
+++ HangMan/app/src/main/java/com/example/hangman/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.hangman;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
-import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
     ImageView drawing;
+    TextView text;
     private dictionary dict;
-    private ArrayList<String> ana;
+    private ArrayList<String> words;
+    private String word;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("words.txt");
+            dict = new dictionary(inputStream);
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
+            toast.show();
+        }
         drawing = (ImageView) findViewById(R.id.drawing);
+        text = (TextView) findViewById(R.id.wordText);
+
         Integer wordLength = 5;
+        word = dict.getRandomWord(wordLength);
+        text.setText(word);
+        Log.d("My word is: ", word);
 
         /*
         Queue<Integer> drawing_queue = new LinkedList<Integer>();
